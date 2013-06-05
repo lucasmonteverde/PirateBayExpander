3b6cea9a1e3de04f4808c3f8204b804d630afe7d
Willian Siqueira <presuntinho63@gmail.com>
2013-06-04 23:07:20 -0300

    Ajuste de layout
    Ajuste de layout

--- Layout/js/init.js
+++ Layout/js/init.js
@@ -5,36 +5,4 @@ $(document).ready(function(){
 	var browserW = $(window).width();
 	var browserH = $(window).height();
 	
-	
-	
-	/*MENU ##################################*/
-	$('#menuTab a').click(function(e) {
-        e.preventDefault();
-		var subMenu = $($(this).attr("href"));
-        
-		if (subMenu.is(':hidden')) {
-            subMenu.slideDown("200");
-			$(this).parent().toggleClass('active');
-			
-        } else {
-            subMenu.slideUp("100");
-			$(this).parent().toggleClass('active');
-        }
-		return false;
-    });
-	
-	
-	
-	
-	/* CAROUSEL ##################################*/
-	var instanceOne = new ImageFlow();
-	instanceOne.init({ ImageFlowID: 'carousel', 
-                   reflections: false, 
-                   reflectionP: 0.4,
-				   circular: true, 
-                   slider: false,
-                   captions: false });
-	
-	
-	
 });
