190a334314993a7a1a1b48baafec251976ddaf4a
Willian Siqueira <presuntinho63@gmail.com>
2013-09-05 19:36:10 -0300

    Nova estrutura/Layout TPB Ex
    Nova estrutura/Layout TPB Ex

--- Layout/HTML_v2/js/init.js
+++ Layout/HTML_v2/js/init.js
@@ -1,8 +1,7 @@
 $(document).ready(function(){
 	var browserW = $(window).width();
 	var browserH = $(window).height();
-	
-	
+
 	$('#btnAdSearch').click(function(){
 		$(this).data('clicked',!$(this).data('clicked'));
 			if ($(this).data('clicked')){
@@ -13,7 +12,7 @@ $(document).ready(function(){
 				$('#boxAdSearch').slideToggle('fast', function() {
 					// Animation complete.
 				});
-			}
+		}
     });
 	
 });
