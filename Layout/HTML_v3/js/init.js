96040440a06289a29e4140b99f9b3e322daac02c
Willian Siqueira <presuntinho63@gmail.com>
2014-03-16 20:45:29 -0300

    Ajuste de layout (menu)
    Ajuste de layout (menu)

---
+++ Layout/HTML_v3/js/init.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+	var browserW = $(window).width();
+	var browserH = $(window).height();
+
+	$('#btnAdSearch').click(function(){
+		$(this).data('clicked',!$(this).data('clicked'));
+			if ($(this).data('clicked')){
+				$('#boxAdSearch').slideToggle('slow', function() {
+					// Animation complete.
+				});
+			}else{
+				$('#boxAdSearch').slideToggle('fast', function() {
+					// Animation complete.
+				});
+		}
+    });
+	
+});
